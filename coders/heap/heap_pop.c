1d646879099e75e112de16ca8be2a726c80d8f1b
Gaspard Tourdiat <gaspard.tourdiat@gmail.com>
2026-04-07 17:11:44 +0200

    add: heap_pop

--- coders/heap/heap_pop.c
+++ coders/heap/heap_pop.c
@@ -6,10 +6,20 @@
 /*   By: gtourdia <@student.42mulhouse.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/07 16:26:24 by gtourdia          #+#    #+#             */
-/*   Updated: 2026/04/07 16:58:29 by gtourdia         ###   ########.fr       */
+/*   Updated: 2026/04/07 17:11:20 by gtourdia         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../codexion.h"
 
+t_coder	*heap_pop(t_dongle	*dongle)
+{
+	t_dongle	*temp;
 
+	if (dongle->heap[0] == NULL)
+		return (NULL);
+	temp = dongle->heap[0];
+	dongle->heap[0] = dongle->heap[1];
+	dongle->heap[1] = NULL;
+	return (temp);
+}
