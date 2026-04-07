f5cd8206ec8347d5e214a1bcd9e51dfa65988b7d
Gaspard Tourdiat <gaspard.tourdiat@gmail.com>
2026-04-07 13:11:36 +0200

    fix: non-predeclared typedef resulting in loop of undeclared type

--- coders/codexion.h
+++ coders/codexion.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   codexion.h                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: gaspard <gaspard@student.42.fr>            +#+  +:+       +#+        */
+/*   By: gtourdia <@student.42mulhouse.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/03 07:19:28 by gtourdia          #+#    #+#             */
-/*   Updated: 2026/04/04 20:31:25 by gaspard          ###   ########.fr       */
+/*   Updated: 2026/04/07 13:10:46 by gtourdia         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,6 +22,9 @@
 # include <time.h>
 # include <sys/time.h>
 
+typedef struct s_dongle	t_dongle; // Pre-declaration
+typedef struct s_coder	t_coder; // Pre-declaration
+
 typedef struct s_args
 {
 	int		nb_coders;
@@ -34,15 +37,6 @@ typedef struct s_args
 	char	*scheduler; // fifo or edf
 }	t_args;
 
-typedef struct s_dongle
-{
-	long			cooldown; // time in ms of last use
-	int				dongle_id; // id of the dongle
-	pthread_mutex_t	mutex; // lock/unlock the right to use it
-	pthread_cond_t	condition; // wait on that condition to broadcast
-	int				is_in_use; // 1 if a coder is using it
-}	t_dongle;
-
 typedef struct s_coder
 {
 	int			coder_id;
@@ -51,6 +45,16 @@ typedef struct s_coder
 	t_dongle	*left_dongle;
 	t_dongle	*right_dongle;
 }	t_coder;
+
+typedef struct s_dongle
+{
+	long			cooldown; // time in ms of last use
+	int				dongle_id; // id of the dongle
+	pthread_mutex_t	mutex; // lock/unlock the right to use it
+	pthread_cond_t	condition; // wait on that condition to broadcast
+	int				is_in_use; // 1 if a coder is using it
+	t_coder			*heap;
+}	t_dongle;
 
 typedef struct s_manager
 {
